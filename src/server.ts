78879a8a2f4ed92fd19d01b047837a16f4c3f860
Magno Belloni <bellonidesouza6@gmail.com>
2020-11-11 22:23:55 -0300

    Add server

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+
+import express, { json, Request, Response, NextFunction } from 'express';
+
+// import cors from 'cors';
+
+import AppError from './errors/AppError';
+import routes from './routes';
+
+const app = express();
+
+// app.use(cors());
+app.use(json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message,
+        });
+    }
+
+    return response.status(500).json({
+        message: 'Internal server error',
+    });
+});
+
+app.use((request: Request, response: Response, _: NextFunction) => {
+    return response.status(404).json({
+        mensagem: 'Rota não encontrada',
+    });
+});
+
+app.listen(3333, () => {
+    console.log('Server started!!');
+});
